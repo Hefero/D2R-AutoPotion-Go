c3439aded1c654952869e4810794e3ce8279d938
Hefero <guilherme.marques00@hotmail.com>
2023-10-20 18:02:14 -0300

    lifewatcher initial

---
+++ cmd/lifewatcher/internal/watcher.go
@@ -0,0 +1,80 @@
+package lifewatcher
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
+	"github.com/hectorgimenez/d2go/pkg/data"
+	"github.com/hectorgimenez/d2go/pkg/data/area"
+	"github.com/hectorgimenez/d2go/pkg/data/item"
+	"github.com/hectorgimenez/d2go/pkg/memory"
+	"github.com/hectorgimenez/d2go/pkg/nip"
+)
+
+type Watcher struct {
+	gr                     *memory.GameReader
+	rules                  []nip.Rule
+	alreadyNotifiedItemIDs []itemFootprint
+}
+
+type itemFootprint struct {
+	detectedAt time.Time
+	area       area.Area
+	position   data.Position
+	name       item.Name
+	quality    item.Quality
+}
+
+func (fp itemFootprint) Match(area area.Area, i data.Item) bool {
+	return fp.area == area && fp.position == i.Position && fp.name == i.Name && fp.quality == i.Quality
+}
+
+func NewWatcher(gr *memory.GameReader, rules []nip.Rule) *Watcher {
+	return &Watcher{gr: gr, rules: rules}
+}
+
+func (w *Watcher) Start(ctx context.Context) error {
+	w.alreadyNotifiedItemIDs = make([]itemFootprint, 0)
+	audioBuffer, err := initAudio()
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			time.Sleep(100 * time.Millisecond)
+
+			d := w.gr.GetData()
+
+			log.Printf("%s: Life: %s", time.Now().Format(time.RFC3339), d.PlayerUnit.Life.ToString())
+
+			speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
+		}
+	}
+}
+
+func initAudio() (*beep.Buffer, error) {
+	f, err := os.Open("assets/ching.wav")
+	if err != nil {
+		return nil, err
+	}
+
+	streamer, format, err := wav.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	buffer := beep.NewBuffer(format)
+	buffer.Append(streamer)
+	streamer.Close()
+
+	return buffer, nil
+}
---
+++ cmd/lifewatcher/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/hectorgimenez/d2go/pkg/memory"
+	"github.com/hectorgimenez/d2go/pkg/nip"
+	lifewatcher "github.com/hefero/d2go/cmd/lifewatcher/internal"
+)
+
+func main() {
+	process, err := memory.NewProcess()
+	if err != nil {
+		log.Fatalf("error starting process: %s", err.Error())
+	}
+
+	gr := memory.NewGameReader(process)
+
+	rules, err := nip.ReadDir("config/itemfilter/")
+	if err != nil {
+		log.Fatalf("error reading NIP files: %s", err.Error())
+	}
+
+	watcher := lifewatcher.NewWatcher(gr, rules)
+
+	ctx := contextWithSigterm(context.Background())
+	err = watcher.Start(ctx)
+	if err != nil {
+		log.Fatalf("error during process: %s", err.Error())
+	}
+}
+
+func contextWithSigterm(ctx context.Context) context.Context {
+	ctxWithCancel, cancel := context.WithCancel(ctx)
+	go func() {
+		defer cancel()
+
+		signalCh := make(chan os.Signal, 1)
+		signal.Notify(signalCh, os.Interrupt)
+
+		select {
+		case <-signalCh:
+		case <-ctx.Done():
+		}
+	}()
+
+	return ctxWithCancel
+}
