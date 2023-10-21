aedd20aaf22fc415912134c6f4090f9477c008e3
Hefero <guilherme.marques00@hotmail.com>
2023-10-20 23:02:55 -0300

    first ver

--- cmd/lifewatcher/internal/watcher.go
+++ cmd/lifewatcher/internal/watcher.go
@@ -31,6 +31,21 @@ type itemFootprint struct {
 	quality    item.Quality
 }
 
+type Manager struct {
+	lastRejuv     time.Time
+	lastRejuvMerc time.Time
+	lastHeal      time.Time
+	lastMana      time.Time
+	lastMercHeal  time.Time
+}
+
+const (
+	healingInterval     = time.Second * 4
+	healingMercInterval = time.Second * 6
+	manaInterval        = time.Second * 5
+	rejuvInterval       = time.Second * 2
+)
+
 func (fp itemFootprint) Match(area area.Area, i data.Item) bool {
 	return fp.area == area && fp.position == i.Position && fp.name == i.Name && fp.quality == i.Quality
 }
@@ -40,9 +55,9 @@ func NewWatcher(gr *memory.GameReader, rules []nip.Rule) *Watcher {
 }
 
 func (w *Watcher) Start(ctx context.Context) error {
+	var manager = Manager{}
 	w.alreadyNotifiedItemIDs = make([]itemFootprint, 0)
 	audioBuffer, err := initAudio()
-	kb, err := keybd_event.NewKeyBonding()
 	if err != nil {
 		return err
 	}
@@ -58,24 +73,54 @@ func (w *Watcher) Start(ctx context.Context) error {
 
 			HPPercent := d.PlayerUnit.HPPercent()
 			MPPercent := d.PlayerUnit.MPPercent()
-			MercHPPercent := d.MercHPPercent()
 
 			log.Printf("%s: Life: %d Mana: %d", time.Now().Format(time.RFC3339), HPPercent, MPPercent)
 
-			if HPPercent < 90 {
-				kb.HasSHIFT(false)
-				kb.SetKeys(keybd_event.VK_1)
-				err = kb.Launching()
+			usedRejuv := false
+			if time.Since(manager.lastRejuv) > rejuvInterval && (d.PlayerUnit.HPPercent() <= 40 || d.PlayerUnit.MPPercent() < 30) {
+				UseRejuv()
+				if usedRejuv {
+					manager.lastRejuv = time.Now()
+				}
 				speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
 			}
 
-			if MercHPPercent < 70 {
-				kb.SetKeys(keybd_event.VK_1)
-				kb.HasSHIFT(true)
-				err = kb.Launching()
-				kb.HasSHIFT(false)
-				speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
+			if !usedRejuv {
+
+				if d.PlayerUnit.HPPercent() <= 85 && time.Since(manager.lastHeal) > healingInterval {
+					UseHP()
+					manager.lastHeal = time.Now()
+					speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
+				}
+
+				if d.PlayerUnit.MPPercent() <= 65 && time.Since(manager.lastMana) > manaInterval {
+					UseMana()
+					manager.lastMana = time.Now()
+					speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
+				}
 			}
+
+			// Mercenary
+			if d.MercHPPercent() > 0 {
+				usedMercRejuv := false
+				if time.Since(manager.lastRejuvMerc) > rejuvInterval && d.MercHPPercent() <= 30 {
+					UseMercRejuv()
+					if usedMercRejuv {
+						manager.lastRejuvMerc = time.Now()
+					}
+					speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
+				}
+
+				if !usedMercRejuv {
+
+					if d.MercHPPercent() <= 60 && time.Since(manager.lastMercHeal) > healingMercInterval {
+						UseHPMerc()
+						manager.lastMercHeal = time.Now()
+						speaker.Play(audioBuffer.Streamer(0, audioBuffer.Len()))
+					}
+				}
+			}
+
 		}
 	}
 }
@@ -96,4 +141,51 @@ func initAudio() (*beep.Buffer, error) {
 	streamer.Close()
 
 	return buffer, nil
+}
+
+func UseHP() {
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+	}
+	kb.HasSHIFT(false)
+	kb.SetKeys(keybd_event.VK_1)
+	err = kb.Launching()
+}
+
+func UseMana() {
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+	}
+	kb.HasSHIFT(false)
+	kb.SetKeys(keybd_event.VK_4)
+	err = kb.Launching()
+}
+
+func UseHPMerc() {
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+	}
+	kb.HasSHIFT(true)
+	kb.SetKeys(keybd_event.VK_1)
+	err = kb.Launching()
+	kb.HasSHIFT(false)
+}
+
+func UseMercRejuv() {
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+	}
+	kb.HasSHIFT(true)
+	kb.SetKeys(keybd_event.VK_2)
+	err = kb.Launching()
+	kb.HasSHIFT(false)
+}
+
+func UseRejuv() {
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+	}
+	kb.HasSHIFT(false)
+	kb.SetKeys(keybd_event.VK_2)
+	err = kb.Launching()
 }
